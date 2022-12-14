19bcbf6604a7e543300f663c9875d8b096218795
thong <thongtrinh49@gmail.com>
2022-12-14 09:54:42 +0700

    change 8080 to 3000

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
