742461e47d345c00e9f556348aac62df07a4844d
thong <thongtrinh49@gmail.com>
2022-12-21 11:55:49 +0700

    deploy contract

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./models");
 
-// db.sequelize.sync();
+db.sequelize.sync();
 // force: true //will drop the table if it already exists
-db.sequelize.sync({ force: true }).then(() => {
-    console.log('Drop and Resync Database with { force: true }');
-    initial();
-});
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log('Drop and Resync Database with { force: true }');
+//     initial();
+// });
 
 // simple route
 app.get("/", (req, res) => {
