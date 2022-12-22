51b3a6474a0ff8c4e10a33b4f9a79c8f1b2e1b18
thong <thongtrinh49@gmail.com>
2022-12-22 16:21:13 +0700

    fix sign in

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -19,5 +19,5 @@ module.exports = function (app) {
   //   controller.signup
   // );
 
-  app.post("/api/auth/signin", controller.signin);
+  app.post("/api/v1/auth/signin", controller.signin);
 };
---
+++ seeders/20221222083210-bookingreservation.js
@@ -0,0 +1,122 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert(
+      "bookingreservation", [{
+        userid: 1,
+        customerid: 1,
+        lobbyid: 1,
+        // statusid: 1,
+        eventid: 1,
+        numberoftables: 1,
+        numberofguests: 3,
+        set: 2,
+        description: "new",
+        fromdate: "2022-01-17T03:23:35.266Z",
+	      todate: "2022-02-17T07:23:35.266Z",
+        menuid: 1,
+        decoration: "san khau",
+        sound: "có",
+        light: "có",
+        mc: "có",
+        contentdisplay: "welcome to ...",
+        singer: "có",
+        entertainment: "diễn xiếc",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userid: 1,
+        customerid: 1,
+        lobbyid: 1,
+        // statusid: 1,
+        eventid: 1,
+        numberoftables: 1,
+        numberofguests: 3,
+        set: 2,
+        description: "new",
+        fromdate: "2022-02-17T03:23:35.266Z",
+	      todate: "2022-03-17T07:23:35.266Z",
+        menuid: 1,
+        decoration: "san khau",
+        sound: "có",
+        light: "có",
+        mc: "có",
+        contentdisplay: "welcome to ...",
+        singer: "có",
+        entertainment: "diễn xiếc",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userid: 1,
+        customerid: 1,
+        lobbyid: 1,
+        // statusid: 1,
+        eventid: 1,
+        numberoftables: 1,
+        numberofguests: 3,
+        set: 2,
+        description: "new",
+        fromdate: "2022-03-17T03:23:35.266Z",
+	      todate: "2022-04-17T07:23:35.266Z",
+        menuid: 1,
+        decoration: "san khau",
+        sound: "có",
+        light: "có",
+        mc: "có",
+        contentdisplay: "welcome to ...",
+        singer: "có",
+        entertainment: "diễn xiếc",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userid: 1,
+        customerid: 1,
+        lobbyid: 1,
+        // statusid: 1,
+        eventid: 1,
+        numberoftables: 1,
+        numberofguests: 3,
+        set: 2,
+        description: "new",
+        fromdate: "2022-04-17T03:23:35.266Z",
+	      todate: "2022-05-17T07:23:35.266Z",
+        menuid: 1,
+        decoration: "san khau",
+        sound: "có",
+        light: "có",
+        mc: "có",
+        contentdisplay: "welcome to ...",
+        singer: "có",
+        entertainment: "diễn xiếc",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {}
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("bookingreservation", null, {});
+
+  }
+};
